128346f5bb02d148743f5e7573acb0342d3758f9
Warut Surapat <s.warut@gmail.com>
2018-01-19 03:45:48 +0900

    Small refactor.

--- sock-merchant/solution.exs
+++ sock-merchant/solution.exs
@@ -2,7 +2,7 @@
 # Ideas:
 #   - Very straight-forward. Just count the number of sock then count number of pair.
 defmodule Solution do
-  def sol(socks) do
+  def solve(socks) do
     socks
     |> Enum.reduce(%{}, fn(item, acc) ->
       case Map.has_key?(acc, item) do
@@ -19,5 +19,4 @@ end
 
 n = String.trim(IO.gets("Input number of sock:")) |> String.to_integer
 socks = String.trim(IO.gets("Input sock list:")) |> String.split(" ") |> Enum.map(fn(y) -> String.to_integer(y) end)
-r = Solution.sol(socks)
-IO.puts("#{inspect r}")
+IO.puts("#{inspect Solution.solve(socks)}")
