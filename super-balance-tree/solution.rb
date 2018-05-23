894c0e7be3f0812609e13fbbfe225c4dd0f6b011
Warut Surapat <s.warut@gmail.com>
2018-05-23 23:05:48 +0900

    Update traverse method.

--- super-balance-tree/solution.rb
+++ super-balance-tree/solution.rb
@@ -1,9 +1,41 @@
 class Solution
   def self.is_super_balanced(tree)
-
+    depts = traverse_and_get_leaf_depths(tree)
   end
 
-  def self.traverse(tree)
-    # if tree.
+  def self.traverse_and_get_leaf_depths(tree, depth = 0, include_node_value = false)
+    if tree.has_left? && tree.has_right?
+      return [
+        traverse_and_get_leaf_depths(
+          tree.left,
+          depth + 1,
+          include_node_value
+        ),
+        traverse_and_get_leaf_depths(
+          tree.right,
+          depth + 1,
+          include_node_value
+        )
+      ].flatten
+    elsif tree.has_left? && !tree.has_right?
+      return traverse_and_get_leaf_depths(
+        tree.left,
+        depth + 1,
+        include_node_value
+      )
+
+    elsif !tree.has_left? && tree.has_right?
+      return traverse_and_get_leaf_depths(
+        tree.right,
+        depth + 1,
+        include_node_value
+      )
+    else
+      if include_node_value
+        return "#{tree.value}, #{depth}"
+      else
+        return depth
+      end
+    end
   end
 end
--- super-balance-tree/spec/solution_spec.rb
+++ super-balance-tree/spec/solution_spec.rb
@@ -1,24 +1,27 @@
 require_relative '../solution'
 
-RSpec.describe Solution, "" do
-  context "1" do
-    it "should be true" do
-      #        1
-      #      2   3
-      #    4  5
-      s = Solution.new
-      b1 = BinaryTreeNode.new("b1")
-      b2 = BinaryTreeNode.new("b2")
-      b3 = BinaryTreeNode.new("b3")
-      b4 = BinaryTreeNode.new("b4")
-      b5 = BinaryTreeNode.new("b5")
-      b2.insert_left(b4)
-      b2.insert_right(b5)
-      b1.insert_left(b2)
-      b1.insert_right(b3)
+RSpec.describe Solution, "traverse_and_get_leaf_depths" do
+  it "collects leaf nodes with depth" do
+    b1 = BinaryTreeNode.new("b1")
+    b2 = b1.insert_left("b2")
+    b2.insert_left("b3")
+    b4 = b2.insert_right("b4")
+    b4.insert_left("b5")
+    b4.insert_right("b6")
 
+    result = Solution.traverse_and_get_leaf_depths(b1, 0, true)
+    expect(result).to eq ["b3, 2", "b5, 3", "b6, 3"]
+  end
 
-      expect(Solution.is_super_balanced(b1)).to eq true
-    end
+  it "collects depth of leaf nodes" do
+    b1 = BinaryTreeNode.new("b1")
+    b2 = b1.insert_left("b2")
+    b2.insert_left("b3")
+    b4 = b2.insert_right("b4")
+    b4.insert_left("b5")
+    b4.insert_right("b6")
+
+    result = Solution.traverse_and_get_leaf_depths(b1)
+    expect(result).to eq [2, 3, 3]
   end
 end
