72b28c68cc8b0158e6efaa1c8e78ee28d0a910ae
Warut Surapat <s.warut@gmail.com>
2018-05-24 23:57:51 +0900

    Implement is_super_balanced.

--- super-balance-tree/solution.rb
+++ super-balance-tree/solution.rb
@@ -1,6 +1,19 @@
 class Solution
   def self.is_super_balanced(tree)
     depts = traverse_and_get_leaf_depths(tree)
+    result = depts.inject({result: true, last: nil, acc: 0}) do |acc, item|
+      if !acc[:last]
+        acc[:last] = item
+      else
+        sum_of_diff = acc[:acc] + (acc[:last] - item)
+        if (sum_of_diff > 1 || sum_of_diff < -1)
+          acc[:result] = false
+        end
+      end
+      acc
+    end
+
+    return result[:result]
   end
 
   def self.traverse_and_get_leaf_depths(tree, depth = 0, include_node_value = false)
--- super-balance-tree/spec/solution_spec.rb
+++ super-balance-tree/spec/solution_spec.rb
@@ -24,4 +24,25 @@ RSpec.describe Solution, "traverse_and_get_leaf_depths" do
     result = Solution.traverse_and_get_leaf_depths(b1)
     expect(result).to eq [2, 3, 3]
   end
+
+  it "correctly check for super balance" do
+    b1 = BinaryTreeNode.new("b1")
+    b2 = b1.insert_left("b2")
+    b2.insert_left("b3")
+    b4 = b2.insert_right("b4")
+    b4.insert_left("b5")
+    b4.insert_right("b6")
+
+    result = Solution.is_super_balanced(b1)
+    expect(result).to eq true
+
+    b1 = BinaryTreeNode.new("b1")
+    b2 = b1.insert_left("b2")
+    b3 = b2.insert_left("b3")
+    b4 = b3.insert_left("b4")
+    b5 = b1.insert_right("b5")
+
+    result = Solution.is_super_balanced(b1)
+    expect(result).to eq false
+  end
 end
