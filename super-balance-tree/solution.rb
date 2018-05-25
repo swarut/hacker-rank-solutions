db82d942c4910993f5e8fe8c6c1f1bbb3da99b80
Warut Surapat <s.warut@gmail.com>
2018-05-25 23:56:14 +0900

    Remove unnecessary return.

--- super-balance-tree/solution.rb
+++ super-balance-tree/solution.rb
@@ -18,7 +18,7 @@ class Solution
 
   def self.traverse_and_get_leaf_depths(tree, depth = 0, include_node_value = false)
     if tree.has_left? && tree.has_right?
-      return [
+      [
         traverse_and_get_leaf_depths(
           tree.left,
           depth + 1,
@@ -31,23 +31,23 @@ class Solution
         )
       ].flatten
     elsif tree.has_left? && !tree.has_right?
-      return traverse_and_get_leaf_depths(
+      traverse_and_get_leaf_depths(
         tree.left,
         depth + 1,
         include_node_value
       )
 
     elsif !tree.has_left? && tree.has_right?
-      return traverse_and_get_leaf_depths(
+      traverse_and_get_leaf_depths(
         tree.right,
         depth + 1,
         include_node_value
       )
     else
       if include_node_value
-        return "#{tree.value}, #{depth}"
+        "#{tree.value}, #{depth}"
       else
-        return depth
+        depth
       end
     end
   end
